eb7a5ed6633d28b4c9646b1a0dea1880bd67c798
P3RD3 <sasho32112@gmail.com>
2023-11-29 15:58:36 +0200

    HM4 task 3/4 push

---
+++ src/Homework_4/Third_Forth_task.java
@@ -0,0 +1,58 @@
+package Homework_4;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ThirdTask {
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean retry = false;
+       while(!retry)
+       {
+           int a;
+           try
+           {
+               System.out.print("Please enter the size of the array: \n");
+               a = scanner.nextInt();
+               int[] numbers = new int[a];
+               for (int i = 0; i < a; i++)
+               {
+                   System.out.printf("Please enter the value in index: %d \n",i);
+                   numbers[i] = scanner.nextInt();
+               }
+               retry = true;
+               sumOddEven(numbers);
+           }
+           catch (InputMismatchException e)
+           {
+               System.out.println("This is not a number!");
+               retry = false;
+           }
+       }
+    }
+    private static void sumOddEven(int[] array)
+    {
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 0; i < array.length; i++ )
+        {
+            int element = array[i];
+            if (element%2==0)
+            {
+                System.out.printf("Element %d has value of %d and is even.\n",i ,element);
+                sumEven += element;
+
+            }
+            else
+            {
+                sumOdd += element;
+                System.out.printf("Element %d has value of %d is odd.\n",i, element);
+            }
+        }
+        System.out.printf("Sum of all even numbers is: %d \n", sumEven);
+        System.out.printf("Sum of all odd numbers is: %d \n", sumOdd);
+    }
+
+}
